9a4228fd2db637f27bea7bbf0f54e849b7c77096
Ajay <ajaykommana123@gmail.com>
2025-01-31 22:50:39 +0530

    LC407 Day20 25

---
+++ 2661_First-Completely-Painted-Row-or-Column.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        location = {}
+        m = len(mat)
+        n =  len(mat[0])
+        for i in range(m):
+            for j in range(n):
+                location[mat[i][j]] = (i,j)
+        rows = [0] * m
+        cols = [0] * n
+        for i in range(len(arr)):
+            r,c = location[arr[i]]
+            rows[r] += 1
+            cols[c] += 1
+            if rows[r] == n or cols[c] == m:
+                return i
