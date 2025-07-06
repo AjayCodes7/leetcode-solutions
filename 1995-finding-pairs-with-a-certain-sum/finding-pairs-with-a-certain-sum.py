8ede2067d8d5ecdc0ce53486be1e5d261f9ffc82
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-07-06 22:53:48 +0530

    Time: 143 ms (96.35%) | Memory: 48.3 MB (82.48%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
@@ -3,20 +3,21 @@ class FindSumPairs:
     def __init__(self, nums1: List[int], nums2: List[int]):
         self.numbers1 = nums1
         self.numbers2 = nums2
-        self.freq = Counter(self.numbers2)
+        self.freq1 = Counter(self.numbers1)
+        self.freq2 = Counter(self.numbers2)
 
     def add(self, index: int, val: int) -> None:
-        self.freq[self.numbers2[index]] -= 1
+        self.freq2[self.numbers2[index]] -= 1
         self.numbers2[index] += val
-        self.freq[self.numbers2[index]] += 1
+        self.freq2[self.numbers2[index]] += 1
 
     def count(self, tot: int) -> int:
         count = 0
-        for i in self.numbers1:
-            if i >= tot:
+        for num, freq in self.freq1.items():
+            if num >= tot:
                 continue
-            if tot - i in self.freq:
-                count += self.freq[tot - i]
+            if tot - num in self.freq2:
+                count += self.freq2[tot - num] * freq
         return count
 
 
