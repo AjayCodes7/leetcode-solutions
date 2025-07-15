7d9fdb01a932d803d8b302561f18735d9005569c
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-07-15 10:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.06%) - LeetSync

---
+++ 3396-valid-word/valid-word.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        # Check if length of word atleast 3
+        if not (len(word) > 2):
+            return False
+        # Check if there is a special character
+        if '@' in word or '#' in word  or '$' in word :
+            return False
+        # Check if there is a vowel
+        if not any( i.lower() in ('a','e','i','o','u') for i in word):
+            return False
+        # Check if there is a consonant
+        if not any(i.isalpha() and i.lower() not in ('a','e','i','o','u') for i in word):
+            return False
+        # All conditions saticified
+        return True
+        
