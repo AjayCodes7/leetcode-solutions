957b13ecf9eea4c87c321e9458873026f6b4a27c
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-08-11 17:32:15 +0530

    Time: 183 ms (31.06%) | Memory: 47.5 MB (65.84%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.py
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.py
@@ -1,15 +1,11 @@
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         mod = 10**9 + 7
-        binStr = bin(n)[2:]
         powers = []
-        p = 0
-        for i in binStr[::-1]:
-            if i == '1':
-                powers.append(2**p)
-            p += 1
+        for i, val in enumerate(bin(n)[2:][::-1]):
+            if val == '1':
+                powers.append(2**i)
         answer = []
-        print(powers)
         for query in queries:
             temp = powers[query[0]]
             for i in range(query[0]+1,query[1]+1):
