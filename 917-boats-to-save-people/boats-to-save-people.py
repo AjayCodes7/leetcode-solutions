aedb11a0dbf25f59318a2a0ade1ee68f13013798
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-08-12 13:13:59 +0530

    Time: 52 ms (20.54%) | Memory: 23.3 MB (59.12%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -2,12 +2,15 @@ class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
         boats = 0
-        while people:
+        i, j = 0, len(people)-1
+        while i <= j:
             boats += 1
-            weight = people.pop()
-            if people and limit - weight >= people[-1]:
-                people.pop()
-                return (ceil(len(people)/2)) + boats
-            elif people and limit - weight >= people[0]:
-                people.pop(0)
+            weight = people[j]
+            j -= 1
+            if people and limit - weight >= people[j]:
+                j -= 1
+                # print("From Here")
+                # return ceil(j-i+1/2) + boats
+            elif people and limit - weight >= people[i]:
+                i += 1
         return boats
