4d1e2eaa96dbefbacdee39b80542636b7d56a30c
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-08-13 05:54:48 +0530

    Time: 28 ms (12.32%) | Memory: 17.9 MB (43.46%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -3,4 +3,6 @@ class Solution:
         if n <= 0:
             return False
         exponent = log(n, 3)
-        return abs(exponent - round(exponent)) < 1e-10
+        print(exponent)
+        # return abs(exponent - round(exponent)) < 1e-10
+        return 3**int(exponent) == n or 3**int(exponent + 1e-10) == n
