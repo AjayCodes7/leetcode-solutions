6324a4a3f2b93b8fdf2c01a3729667cb5c459f01
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-08-28 17:13:15 +0530

    Time: 15 ms (37.92%) | Memory: 18 MB (9.17%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -0,0 +1,52 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        # Should have to write a method that takes a list and make it sorted
+        #  - ASC : if y != 0
+        #  - DSC : if y == 0
+        
+        # Traverse form right -> left(x = 0) and then top -> bottom (y = 0)
+        # Need to create a temporary list of every diagonal
+        # get the sorted list and update the grid
+        n = len(grid)
+
+        x = 0
+        for y in range(n-1, -1,-1):
+            i = x
+            j = y
+            temp = []
+            while i < n and j < n:
+                temp.append(grid[i][j])
+                i += 1
+                j += 1
+            temp.sort()
+            idx = 0
+
+            i = x
+            j = y
+            while i < n and j < n:
+                grid[i][j] = temp[idx]
+                idx += 1
+                i += 1
+                j += 1
+            
+        y = 0
+        for x in range(n-1, -1,-1):
+            i = x
+            j = y
+            temp = []
+            while i < n and j < n:
+                temp.append(grid[i][j])
+                i += 1
+                j += 1
+
+            temp.sort(reverse = True)
+            idx = 0
+            i = x
+            j = y
+            while i < n and j < n:
+                grid[i][j] = temp[idx]
+                idx += 1
+                i += 1
+                j += 1
+            
+        return grid
