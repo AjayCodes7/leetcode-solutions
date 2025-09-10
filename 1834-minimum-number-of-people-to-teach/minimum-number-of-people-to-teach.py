4aa8714f939d527284561efbe726d51a70a0c97a
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-09-10 14:24:19 +0530

    Time: 51 ms (86.59%) | Memory: 27.2 MB (92.68%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.py
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.py
@@ -2,18 +2,18 @@ class Solution:
     def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
         cntcom = set()
         for friendship in friendships:
-            ccom = {}
-            conm = False
+            cancom = {}
+            cantcom = False
             for lang in languages[friendship[0]-1]:
-                ccom[lang] = 1
+                cancom[lang] = 1
             for lang in languages[friendship[1]-1]:
-                if lang in ccom:
-                    conm = True
+                if lang in cancom:
+                    cantcom = True
                     break
-            if not conm:
+            if not cantcom:
                 cntcom.add(friendship[0]-1)
                 cntcom.add(friendship[1]-1)
-        
+
         max_count = 0
         count = [0] * (n+1)
         for friendship in cntcom:
@@ -22,4 +22,3 @@ class Solution:
                 max_count = max(max_count, count[lang])
         return len(cntcom) - max_count
 
-        
