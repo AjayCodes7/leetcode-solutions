b91506e76140e981a875fc1118b783bb45404907
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-09-12 15:47:18 +0530

    Time: 55 ms (18.79%) | Memory: 18 MB (95.15%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,9 +1,6 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        count = 0
         for ch in s:
             if ch in ['a','e','i','o','u']:
-                count += 1
-        if count == 0:
-            return False
-        return True
+                return True
+        return False
