334f8e07b63b43d46851ec33cc5f350452dd2e15
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-09-19 18:51:43 +0530

    Time: 2 ms (2.68%) | Memory: 42.8 MB (5.42%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    public static int binSearchList(int[][] matrix, int target){
+        int left = 0;
+        int right = matrix.length - 1;
+        int mid;
+        int n = matrix[0].length;
+        while (left <= right){
+            mid = (left + right)/2;
+            if(target < matrix[mid][0]){
+                right = mid - 1;
+            } else if (target > matrix[mid][n-1]){
+                left = mid + 1;
+            } else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean binSearch(int[] matrix, int target){
+        int left = 0;
+        int right = matrix.length - 1;
+        int mid;
+        while(left <= right){
+            mid = (left + right)/2;
+            if(target == matrix[mid]){
+                return true;
+            } else if(target < matrix[mid]){
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int idx = binSearchList(matrix, target);
+        System.out.println(idx);
+        if(idx == -1){
+            return false;
+        }
+        return binSearch(matrix[idx], target);
+    }
+}
