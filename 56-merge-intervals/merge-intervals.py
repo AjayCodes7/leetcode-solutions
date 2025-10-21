67c1dbe1459e0896e110478cc2b88e82d258cf17
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-10-21 18:21:41 +0530

    Time: 15 ms (9.68%) | Memory: 21 MB (64.86%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -5,7 +5,8 @@ class Solution:
         i = 0
         while i < len(intervals) - 1:
             if intervals[i+1][0] <= intervals[i][1]:
-                intervals[i][1] = max(intervals[i+1][1], intervals[i][1])
+                if intervals[i+1][1] > intervals[i][1]:
+                    intervals[i][1] = intervals[i+1][1]
                 intervals.pop(i+1)
                 continue
             i += 1
