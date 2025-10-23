467b6d43268bce9cb4bb968ce54ce4790df2278b
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-10-23 17:32:32 +0530

    Time: 327 ms (97.71%) | Memory: 40.2 MB (60.29%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.py
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.py
@@ -1,17 +1,29 @@
 class Solution:
     def maximumTotalDamage(self, power: List[int]) -> int:
-        count = Counter(power)
-        vec = [(-(10**9), 0)]
-        for k in sorted(count.keys()):
-            vec.append((k, count[k]))
-        n = len(vec)
-        f = [0] * n
-        mx = 0
-        j = 1
-        for i in range(1, n):
-            while j < i and vec[j][0] < vec[i][0] - 2:
-                mx = max(mx, f[j])
-                j += 1
-            f[i] = mx + vec[i][0] * vec[i][1]
-        return max(f)
+        freq = Counter(power)
+        keys = sorted(freq.keys())
+
+        N = len(keys)
+        if N == 1:
+            return keys[0] * freq[keys[0]]
+        if N == 2:
+            if keys[0] + 2 < keys[1]:
+                return keys[0] * freq[keys[0]] + keys[1] * freq[keys[1]]
+            else:
+                return max(keys[0] * freq[keys[0]], keys[1] * freq[keys[1]])
         
+        dp = [0]*N
+        dp[0] = keys[0] * freq[keys[0]]
+        dp[1] = keys[0] * freq[keys[0]] + keys[1] * freq[keys[1]] if keys[0] + 2 < keys[1] else max(keys[0] * freq[keys[0]], keys[1] * freq[keys[1]])
+
+        for i in range(2, N):
+            if keys[i-1]+2 < keys[i]:
+                dp[i] = dp[i-1] + keys[i] * freq[keys[i]]
+            elif keys[i-2] + 2 < keys[i]:
+                dp[i] = max(dp[i-1], dp[i-2] + keys[i] * freq[keys[i]])
+            else:
+                if i < 3:
+                    dp[i] = max(dp[i-1], keys[i] * freq[keys[i]])
+                else:
+                    dp[i] = max(dp[i-1], dp[i-3] + keys[i] * freq[keys[i]])
+        return dp[-1]
