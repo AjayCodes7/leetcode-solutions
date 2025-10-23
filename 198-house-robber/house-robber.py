9c44bbb8ee44e2b5946279229b3a4124e8d3c032
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-10-23 16:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.17%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
-        if N == 1:
-            return nums[0]
-        if N == 2:
-            return max(nums[0], nums[1])
         dp = [0]*N
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, N):
-            consider = nums[i] + dp[i-2]
-            not_consider = dp[i-1]
-            dp[i] = max(consider, not_consider)
+        for i in range(1, N):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
         return dp[-1]
