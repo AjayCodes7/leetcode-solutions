67a120352601b074aad1cc4bf419f171eb60f6ad
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-10-27 20:01:56 +0530

    Time: 41 ms (54.27%) | Memory: 26.7 MB (75.36%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -3,8 +3,6 @@ class Solution:
         prev = target[0]
         for i in range(1, len(target)):
             curr = target[i]
-            if curr <= target[i-1]:
-                continue
-            else:
+            if curr > target[i-1]:
                 prev += curr - target[i-1]
         return prev
