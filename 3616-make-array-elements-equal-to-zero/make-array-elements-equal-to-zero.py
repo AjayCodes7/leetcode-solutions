47432c9f23d5bcc3a9c05526ac21b52a7495a67f
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-10-28 10:28:06 +0530

    Time: 44 ms (82.98%) | Memory: 17.8 MB (54.26%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.py
@@ -1,39 +1,46 @@
 class Solution:
 
-    def isValid(self, position):
-        count = 0
-        # left
-        arr = self.nums[:]
-        i = position
-        direct = -1
-        while -1 < i < len(self.nums):
-            if arr[i] > 0:
-                arr[i] -= 1
-                direct = -direct
-            i += direct
-        if arr.count(0) == self.N:
-            count += 1
-        # right
-        arr = self.nums[:]
-        i = position
-        direct = 1
-        while -1 < i < len(self.nums):
-            if arr[i] > 0:
-                arr[i] -= 1
-                direct = -direct
-            i += direct
-        if arr.count(0) == self.N:
-            count += 1
+    # def isValid(self, position):
+    #     count = 0
+    #     # left
+    
+    #     arr = self.nums[:]
+    #     i = position
+    #     direct = -1
+    #     while -1 < i < len(self.nums):
+    #         if arr[i] > 0:
+    #             arr[i] -= 1
+    #             direct = -direct
+    #         i += direct
+    #     if arr.count(0) == self.N:
+    #         count += 1
+    #     # right
+    #     arr = self.nums[:]
+    #     i = position
+    #     direct = 1
+    #     while -1 < i < len(self.nums):
+    #         if arr[i] > 0:
+    #             arr[i] -= 1
+    #             direct = -direct
+    #         i += direct
+    #     if arr.count(0) == self.N:
+    #         count += 1
         
-        return count
+    #     return count
 
 
 
     def countValidSelections(self, nums: List[int]) -> int:
-        self.nums = nums
-        self.N = len(nums)
-        result = 0
-        for i in range(self.N):
-            if nums[i] == 0:
-                result += self.isValid(i)
-        return result
+        total = sum(nums)
+        left_sum = 0
+        count = 0
+        for num in nums:
+            if num == 0:
+                right_sum = total - left_sum
+
+                if right_sum == left_sum:
+                    count += 2
+                elif abs(right_sum - left_sum) == 1:
+                    count += 1
+            left_sum += num
+        return count
