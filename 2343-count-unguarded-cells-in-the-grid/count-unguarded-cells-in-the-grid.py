c7fdd0a05101c513321cfabbcc2d1901615922bd
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-11-02 19:27:32 +0530

    Time: 329 ms (73.47%) | Memory: 39.1 MB (79.08%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -1,41 +1,37 @@
 class Solution:
-    UNGUARD = 0
-    GUARDED = 1
-    GUARD = 2
-    WALL = 3
     def _markGuard(self, i,j,marking):
         # top
         for temp in range(i - 1, -1, -1 ):
-            if marking[temp][j] == self.WALL or marking[temp][j] == self.GUARD:
+            if marking[temp][j] == 3 or marking[temp][j] == 2:
                 break
             marking[temp][j] = 1
 
         # bottom
         for temp in range(i + 1, len(marking)):
-            if marking[temp][j] == self.WALL or marking[temp][j] == self.GUARD:
+            if marking[temp][j] == 3 or marking[temp][j] == 2:
                 break
             marking[temp][j] = 1
 
         # left
         for temp in range(j - 1, -1, -1):
-            if marking[i][temp] == self.WALL or marking[i][temp] == self.GUARD:
+            if marking[i][temp] == 3 or marking[i][temp] == 2:
                 break
             marking[i][temp] = 1
 
         # right
         for temp in range(j + 1, len(marking[0])):
-            if marking[i][temp] == self.WALL or marking[i][temp] == self.GUARD:
+            if marking[i][temp] == 3 or marking[i][temp] == 2:
                 break
             marking[i][temp] = 1
 
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        marking = [[self.UNGUARD]*n for _ in range(m)]
+        marking = [[0]*n for i in range(m)]
 
         for i,j in walls:
-            marking[i][j] = self.WALL
+            marking[i][j] = 3
 
         for i,j in guards:
-            marking[i][j] = self.GUARD
+            marking[i][j] = 2
         
         for i,j in guards:
             self._markGuard(i,j, marking)
