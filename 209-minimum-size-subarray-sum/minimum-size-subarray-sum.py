9c7b4f0102b450a86d6d5316814463f24b06058e
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-11-02 12:11:24 +0530

    Time: 12 ms (30.55%) | Memory: 28.3 MB (37.46%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,7 +5,7 @@ class Solution:
         l = 0 
         r = 0
         cur_sum = 0
-        while r < len(nums):
+        for r in range(len(nums)):
             cur_sum += nums[r]
             while l < len(nums) and cur_sum >= target:
                 if cur_sum >= target:
@@ -13,7 +13,6 @@ class Solution:
                     result = min(result, r-l+1)
                 cur_sum -= nums[l]
                 l += 1
-            r += 1
         if flag:
             return result
         return 0
