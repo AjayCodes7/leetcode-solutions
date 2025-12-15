ce9dac9c7bdf966f5eaf7c5ed4a9408cfebf6685
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-12-15 16:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (59.87%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -14,9 +14,5 @@ var maxDepth = function(root) {
     if(!root){
         return 0;
     }
-
-    let left = maxDepth(root.left);
-    let right = maxDepth(root.right);
-
-    return 1 + Math.max(left, right);
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
