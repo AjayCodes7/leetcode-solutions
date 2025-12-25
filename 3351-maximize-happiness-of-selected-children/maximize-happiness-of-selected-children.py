df082c189d9422a20849f5010673ee1c35a38db7
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-12-25 12:06:25 +0530

    Time: 792 ms (87.94%) | Memory: 43.3 MB (97.16%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -6,6 +6,10 @@ class Solution:
         turns = 0
 
         for i in range(k):
-            total_happiness_sum += max(happiness[i] - turns, 0)
+            # total_happiness_sum += max(happiness[i] - turns, 0)
+            if happiness[i] - turns > 0:
+                total_happiness_sum += happiness[i] - turns
+            else:
+                break
             turns += 1
         return total_happiness_sum
