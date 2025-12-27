95f655c532cfc12edcb38521cfda99fc8911ab83
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2025-12-27 22:53:18 +0530

    Time: 175 ms (19.50%) | Memory: 22.7 MB (31.10%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,21 +1,26 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        n = len(customers)
-        prefix_sum = [0] * (n + 1)
-        for i, customer in enumerate(customers):
-            prefix_sum[i + 1] = prefix_sum[i] + (1 if customer == 'Y' else 0)
-      
-        best_hour = 0
-        min_penalty = float('inf')
-      
-        for closing_hour in range(n + 1):
-            penalty_before = closing_hour - prefix_sum[closing_hour]
-            penalty_after = prefix_sum[-1] - prefix_sum[closing_hour]
-          
-            total_penalty = penalty_before + penalty_after
+        l = len(customers) + 1
+        prefix = [0] * (l)
+        postfix = [0] * (l)
+        for i in range(1, l):
+            if customers[i-1] == 'N':
+                prefix[i] = prefix[i-1] + 1
+            else:
+                prefix[i] = prefix[i-1]
+            
+            if customers[l-i-1] == 'Y':
+                postfix[l-i-1] = postfix[l-i] + 1
+            else:
+                postfix[l-i-1] = postfix[l-i]
+        
+        min_penalty, idx = float('inf'), 0
+        
+        for i in range(l):
+            penalty = postfix[i] + prefix[i]
 
-            if total_penalty < min_penalty:
-                best_hour = closing_hour
-                min_penalty = total_penalty
-      
-        return best_hour
+            if penalty < min_penalty:
+                min_penalty = penalty
+                idx = i
+
+        return idx
